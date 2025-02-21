75b97be4999c660ab830a5ff096ffc3797edeecc
Shivangi <shivangichaurasia1515@gmail.com>
2025-02-22 00:20:48 +0530

    files added

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a plate of dishes crafted with the love and pure ingredients and cooking expertise by our great chefs.Our mission is to satisfy your craving and elevate your dining meal at a time</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
